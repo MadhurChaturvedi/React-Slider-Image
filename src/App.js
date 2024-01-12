bdd2924056f308ed3b3b4dc5cda56c02fb3e05fd
Madhur <madhurchaturvedi2000@gmail.com>
2024-01-12 23:02:40 +0530

    now add the transition

--- src/App.js
+++ src/App.js
@@ -35,7 +35,7 @@ function App() {
     setCurrentIndex(slideIndex);
   };
   return (
-    <div className={ theme ? `max-w-[100%] h-[750px] w-full m-auto py-16 px-4 relative group`:`max-w-[100%] h-[750px] w-full m-auto py-16 px-4 relative group bg-slate-800`}>
+    <div className={ theme ? `max-w-[100%] h-[750px] w-full m-auto py-16 px-4 relative group transition-all duration-150`:`max-w-[100%] h-[750px] w-full m-auto py-16 px-4 relative group bg-slate-800 transition-all duration-150`}>
       <label class="relative inline-flex items-center cursor-pointer justify-center">
         <input type="checkbox" value="" class="sr-only peer"   onClick={handleTheme}/>
         <div class="w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
