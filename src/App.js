d8de6cc8c3efe3014462ed976405e72191eb25e2
Madhur <madhurchaturvedi2000@gmail.com>
2024-01-12 22:58:22 +0530

    the toggle themer is added is the slider app

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import {BsChevronCompactLeft ,BsChevronCompactRight } from 'react-icons/bs';
+import { BsChevronCompactLeft, BsChevronCompactRight } from "react-icons/bs";
 import { RxDotFilled } from "react-icons/rx";
 function App() {
   const slides = [
@@ -14,43 +14,65 @@ function App() {
     },
   ];
 
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const [theme, setTheme] = useState("dark");
 
-  const [currentIndex,setCurrentIndex] =  useState(0);
+  const handleTheme=()=>{
+     setTheme(!theme)
+  }
+  const prevSlid = () => {
+    const isFirstSlid = currentIndex === 0;
+    const newIndex = isFirstSlid ? slides.length - 1 : currentIndex - 1;
+    setCurrentIndex(newIndex);
+  };
+  const nextSlid = () => {
+    const isLastSlid = currentIndex === slides.length - 1;
+    const newIndex = isLastSlid ? 0 : currentIndex + 1;
+    setCurrentIndex(newIndex);
+  };
 
-  const prevSlid  = () =>{
-     const isFirstSlid = currentIndex === 0
-     const newIndex = isFirstSlid ? slides.length -1 :currentIndex - 1;
-     setCurrentIndex(newIndex)
-  }
-  const nextSlid  = () =>{
-    const isLastSlid = currentIndex === slides.length - 1
-    const newIndex = isLastSlid ? 0  :currentIndex + 1;
-    setCurrentIndex(newIndex)
-  }
-
-  const goToSlid =(slideIndex)=>{
-      setCurrentIndex(slideIndex)
-  }
+  const goToSlid = (slideIndex) => {
+    setCurrentIndex(slideIndex);
+  };
   return (
-    <div className="max-w-[1400px] h-[780px] w-full m-auto py-16 px-4 relative group">
-        <div style={{backgroundImage:`url(${slides[currentIndex].url})`}} className="w-full h-full rounded-2xl bg-center  bg-cover duration-500 " >
-            {/* Left Arrow */}
-            <div className=" hidden group-hover:block hover:transition-all transition-all absolute cursor-pointer top-[50%] -translate-x-0 translate-y-[-50%] left-5 text-2xl rounded-full p-2 " style={{transition:'0.3s'}}>
-             <BsChevronCompactLeft onClick={prevSlid}  color="#FFF" size={30}/> 
-            </div>
-            {/* right Arrow */}
-            <div className=" hidden group-hover:block hover:transition-all transition-all absolute cursor-pointer top-[50%] -translate-x-0 translate-y-[-50%] right-5 text-2xl rounded-full p-2 " style={{transition:'0.3s'}}>
-              <BsChevronCompactRight onClick={nextSlid} color="#FFF" size={30} />
-            </div>
-            
+    <div className={ theme ? `max-w-[100%] h-[750px] w-full m-auto py-16 px-4 relative group`:`max-w-[100%] h-[750px] w-full m-auto py-16 px-4 relative group bg-slate-800`}>
+      <label class="relative inline-flex items-center cursor-pointer justify-center">
+        <input type="checkbox" value="" class="sr-only peer"   onClick={handleTheme}/>
+        <div class="w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
+        <span class="ms-3 text-sm font-medium text-gray-900 dark:text-gray-300">
+          Toggle me
+        </span>
+      </label>
+      <div
+        style={{ backgroundImage: `url(${slides[currentIndex].url})` }}
+        className="w-full h-full rounded-2xl bg-center  bg-cover duration-500 "
+      >
+        {/* Left Arrow */}
+        <div
+          className=" hidden group-hover:block hover:transition-all transition-all absolute cursor-pointer top-[50%] -translate-x-0 translate-y-[-50%] left-5 text-2xl rounded-full p-2 "
+          style={{ transition: "0.3s" }}
+        >
+          <BsChevronCompactLeft onClick={prevSlid} color="#FFF" size={30} />
         </div>
-        <div className="flex top-4 justify-center py-2">
-              {slides.map((slide,slideIndex)=>(
-                <div key={slideIndex} onClick={()=>goToSlid(slideIndex)} className="text-2xl cursor-pointer hover:text-red-600 transition-all">
-                  <RxDotFilled/>
-                 </div> 
-              ))}      
-            </div>
+        {/* right Arrow */}
+        <div
+          className=" hidden group-hover:block hover:transition-all transition-all absolute cursor-pointer top-[50%] -translate-x-0 translate-y-[-50%] right-5 text-2xl rounded-full p-2 "
+          style={{ transition: "0.3s" }}
+        >
+          <BsChevronCompactRight onClick={nextSlid} color="#FFF" size={30} />
+        </div>
+      </div>
+      <div className="flex top-4 justify-center py-2">
+        {slides.map((slide, slideIndex) => (
+          <div
+            key={slideIndex}
+            onClick={() => goToSlid(slideIndex)}
+            className="text-2xl cursor-pointer hover:text-red-600 transition-all"
+          >
+            <RxDotFilled />
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
